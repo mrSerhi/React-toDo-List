970abd3c9e55d538f03e927961f2b6ea4ffcbd46
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-02-26 18:09:52 +0200

    Updates

---
+++ precache-manifest.7044600d88dad42646a4f0a75f4b21b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57420722b697482a0445",
+    "url": "/React-toDo-List/static/js/runtime~main.57420722.js"
+  },
+  {
+    "revision": "6d9fa743b41918959950",
+    "url": "/React-toDo-List/static/js/main.6d9fa743.chunk.js"
+  },
+  {
+    "revision": "49632993f820642bff0c",
+    "url": "/React-toDo-List/static/js/2.49632993.chunk.js"
+  },
+  {
+    "revision": "6d9fa743b41918959950",
+    "url": "/React-toDo-List/static/css/main.325c1d9d.chunk.css"
+  },
+  {
+    "revision": "49632993f820642bff0c",
+    "url": "/React-toDo-List/static/css/2.49a585b8.chunk.css"
+  },
+  {
+    "revision": "c503153a3c1555a85d29ee58097dbbb7",
+    "url": "/React-toDo-List/index.html"
+  }
+];
