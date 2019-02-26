82cbedb67b85ad0596bf632d1517d83701f31555
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-02-26 17:49:06 +0200

    product stage

---
+++ build/precache-manifest.bb623bec4a62c05b7abf7ee0868fd64b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6d9fa743b41918959950",
+    "url": "/static/js/main.6d9fa743.chunk.js"
+  },
+  {
+    "revision": "49632993f820642bff0c",
+    "url": "/static/js/2.49632993.chunk.js"
+  },
+  {
+    "revision": "6d9fa743b41918959950",
+    "url": "/static/css/main.325c1d9d.chunk.css"
+  },
+  {
+    "revision": "49632993f820642bff0c",
+    "url": "/static/css/2.49a585b8.chunk.css"
+  },
+  {
+    "revision": "bfaa2d145f66cc93b5cfa0525809633d",
+    "url": "/index.html"
+  }
+];
